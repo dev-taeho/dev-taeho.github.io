d165bb7df59fbc502a2b3cbdabdbffa83b898683
Taehoya <cth6809@naver.com>
2025-04-27 14:44:50 -0500

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click onaa the Vite and React logos to learn more
+        Click onaa thess Vite and React logos to learn more
       </p>
     </>
   );
